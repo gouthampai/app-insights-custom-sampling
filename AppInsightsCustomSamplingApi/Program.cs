8d42002e4b37360e85966421b8fbbd6584d15c85
Goutham Pai <goutham.r.pai@gmail.com>
2025-11-27 01:45:46 -0600

    make code update for testing deployment

--- AppInsightsCustomSamplingApi/Program.cs
+++ AppInsightsCustomSamplingApi/Program.cs
@@ -20,7 +20,7 @@ if (app.Environment.IsDevelopment())
 
 Todo[] sampleTodos =
 [
-    new(1, "Walk the dog"),
+    new(1, "Walk the fish"),
     new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
     new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
     new(4, "Clean the bathroom"),
